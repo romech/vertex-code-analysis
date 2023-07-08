a64448301bc7043ac6b3b4a4ee062b9a21e80969
saltenisdainius97@gmail.com <saltenisdainius97@gmail.com>
2023-07-09 00:59:03 +0200

    improved prompting and sampling

--- ai_test.py
+++ ai_test.py
@@ -27,54 +27,37 @@ with open("service_account.json", encoding="utf-8") as f:
 
 
 # Initialize Vertex AI with project and location
-
 vertexai.init(project=project_id, location="us-central1")
-
-"""chat_model = ChatModel.from_pretrained("code-bison")
-parameters = {
-    "temperature": 0.8,
-    "max_output_tokens": 1024,
-    "top_p": 0.8,
-    "top_k": 40,
-}
-chat = chat_model.start_chat(  # Initialize the chat with model
-    # chat context and examples go here
-)
-
-# Send the human message to the model and get a response
-response = chat.send_message("Are you a human or a machine?", **parameters)
-"""
 
 initialization_prompt = dict()
 initialization_prompt['context'] = """
 You are a very smart Python coding assistant, behaving like an AI linter. You must help the user to improve their code 
-by providing short, human-interpretable coding suggestions.
+by providing short, human-interpretable coding suggestions that improve code speed, quality and correctness.
 """
 initialization_prompt['instructions'] = """
 1. Provide Python coding suggestions that are described in a human-readable language.
-2. Each suggestion that you give must be provided in the following format of a JSON file:
+2. For each line of code please provide the following JSON field of suggestion:
 `
 {
-    "line_start": [integer, the start line of your suggestion in the given code, e.g. 3]
-    "line_end": [integer, the end line of your suggestion's scope, e.g. 3, 4, 6]. Your suggestions can be multi-line.
-    "suggestion": [string, the suggestion that a programmer is supposed to read to improve the code. Preferably one sentence long]
+    "code_line_num": [int, which line of code the suggestion is dedicated to]
+    "suggestion": [string; the suggestion that a programmer is supposed to read to improve the code. Preferably one sentence long]
+    "confidence": [float; confidence score in range of [0, 1] displaying your confidence that your suggestion is useful] 
+    "suggestion_class_number": [integer; improvement regarding: 0- code performance or program; 1: code quality; 2: style or convention compliance; 3: warning for bug / bad practice; 4: error / bug fix; 5: other]
 }
 `
 3. Your output can contain many suggestions.
-4. Your output must be parsable with Python json.loads(). 
-5. Respond with nothing else but a JSON format string containing N number of suggestions.
-6. Your suggestions should not overlap with each other in their scope other or be inside each other.
-7. Your suggestions must be associated with the line of code that they try to improve. Make sure that you cite correct 
+4. Respond with nothing else but a JSON format string containing N number of suggestions. It must be parsable with Python json.loads().
+5. Your suggestions must not overlap with each other in their scope other and not be inside each other.
+6. Your suggestions must be associated with the line of code that they try to improve. Make sure that you cite correct 
 line numbers.
 """
 initialization_prompt['guidelines'] = """
-1. Do not overwhelm the user with too many suggestions, but make sure to provide at least some of them so that the 
-programmer can always improve.
+1. Make sure to provide at least some suggestions for the code input snippet so that the programmer can always improve.
 2. If possible, provide in-built function suggestions or error codes.
-3. Examples of improvement could be, e.g. speed improvements, code quality or style improvements, code amount reduction,
-potential bug or warning notifications. 
-4. Provide short, linter-like suggestions. One sentence preferably.
-5. Only provide suggestions that change the code in some way, not make it the same as it was before.
+3. Provide short, linter-like suggestions. Preferably one sentence long.
+4. Only provide suggestions that change the code in some way, not make it the same as it was before.
+5. Your suggestion should provide a clear instruction of how the code needs to be changed.
+6. IMPORTANT: Be absolutely sure that your suggestion is useful.
 """
 
 example_1_input = """
@@ -119,12 +102,12 @@ example_3_output = """
     "suggestion:" "Use Python in-built `sum` function: `total = sum(integers)` for faster performance and readability"
 }
 """
-initialization_prompt['examples'] = {
-    "explanation": "Examples below display how your outputs might look like. Please only use them for guidance purposes and do not use them directly.",
-    "example_1": [example_1_input, example_1_output],
-    "example_2": [example_2_input, example_2_output],
-    "example_3": [example_3_input, example_3_output]
-}
+#initialization_prompt['examples'] = {
+#    "explanation": "Examples below display how your outputs might look like. Please only use them for guidance purposes and do not use them directly.",
+#    "example_1": [example_1_input, example_1_output],
+#    "example_2": [example_2_input, example_2_output],
+#    "example_3": [example_3_input, example_3_output]
+#}
 #example_prompt['examples'] = [
 #    InputOutputTextPair(
 #        input_text=example_1_input,
@@ -140,27 +123,16 @@ input_prompt["task"] = """
 Provide coding tips for this code as you were instructed.
 """
 input_prompt["code"] = '''
-def performance_test(test_fxn: callable):
-    test_completed = False
+def get_project_root(root_folder: str = 'CoderAgentGPT') -> str:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    total_test_time = 0
-    runs = 0
+    while dir_path != '/':
+        if Path(dir_path).name == root_folder:
+            return dir_path
+        else:
+            dir_path = os.path.dirname(dir_path)
 
-    test_start_time = time.perf_counter()
-
-    while total_test_time < args.total_time:
-        try:
-            time_delta, _ = test_fxn()
-            total_test_time += time_delta
-        except Exception as e:
-            error_stack = traceback.format_exc()
-            return test_completed, error_stack
-        runs += 1
-
-    test_completed = True
-    average_run_time = (time.perf_counter() - test_start_time) / runs
-
-    return test_completed, average_run_time
+    raise Exception('Project root not found.')
 '''
 
 def insert_linenum(source_str, target_str='\n'):
@@ -183,9 +155,9 @@ input_prompt['code'] = insert_linenum(input_prompt['code'])
 #}
 
 parameters = {
-    "temperature": 0.0,
+    "temperature": 0.9,
     "max_output_tokens": 1024,
-    "top_p": 0.9,
+    "top_p": 1.0,
     "top_k": 40,
 }
 
@@ -205,5 +177,4 @@ chat = chat_model.start_chat(
 response = chat.send_message(
     json.dumps(input_prompt), **parameters
 )
-
-print(response)
+a = 5
